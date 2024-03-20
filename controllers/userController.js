3ca2d7794e5c5a83f57f6b12f3b43af25cbbf893
saniyaj mallik <saniyajmallik12@gmail.com>
2024-03-20 21:03:16 +0530

    authentication added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,30 @@
-export const getUserProfile =  (req, res) => {
+import { User } from "../models/userModel.js"
+
+export const getUserProfile = (req, res) => {
     res.send("Profile")
+}
+
+
+
+export const logout = async (req, res) => {
+    try {
+
+        const { email } = req.body
+        let user = await User.findOne({ email })
+        user.token = null;
+        user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "logout successful",
+            token: null
+        })
+
+
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            message: "Cannot logout user"
+        })
+    }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { getUserProfile} from '../controllers/userController.js';
+import { getUserProfile, logout} from '../controllers/userController.js';
+import { googleSignup } from '../controllers/googleSignupController.js';
 
 const router = express.Router();
 
 router.get('/profile',getUserProfile)
+router.post('/google/signup',googleSignup)
+router.post('/logout',logout)
 
 
 export default router
